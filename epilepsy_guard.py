4226d9d8695df82c81b2fcd33e7e1122734a9e36
Kai Wei Mo <devinkwmo@gmail.com>
2021-01-04 16:04:14 -0500

    Changed bot response to be embeds.

--- epilepsy_guard.py
+++ epilepsy_guard.py
@@ -12,21 +12,28 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  for attachment in message.attachments:
-    if attachment.filename.endswith('.gif'):
-        text = message.content
-        author = message.author
-        await message.channel.purge(limit=1)
+    for attachment in message.attachments:
+        if attachment.filename.endswith('.gif'):
+            text = message.content
 
-        if (isMalicious(attachment.proxy_url)): # proxy temporarily persists after deletion
-            await message.channel.send('bad :(')
-        else:
-            await message.channel.send('good :)')
-            # author_full_user = author.name + '#' + author.discriminator
-            # await message.channel.send('**' + author_full_user + '**:\n>>> ' + text)
+            author = message.author
+            author_full_user = author.name + '#' + author.discriminator
+            await message.channel.purge(limit=1)
+
+            if (isMalicious(attachment.proxy_url)): # proxy temporarily persists after deletion
+                e = discord.Embed(title=author_full_user + ' sent a GIF.', description='', color=0xff6962)
+                e.add_field(name="ANALYSIS:", value="We determined that your GIF is capable of inducing epileptic seizures.\nFor the safety of you and your server, we have removed your GIF.", inline=False)
+                e.add_field(name="USER MESSAGE:", value='>>> ' + text, inline=False)
+            else:
+                e = discord.Embed(title=author_full_user + ' sent a GIF.', description='', color=0x77dd76)
+                e.add_field(name="ANALYSIS:", value="This GIF is safe to watch.", inline=False)
+                e.add_field(name="USER MESSAGE:", value='>>> ' + text, inline=False)
+                e.set_image(url=attachment.proxy_url)
+
+            await message.channel.send(embed=e)
 
 
 client.run(CLIENT_SECRET)
